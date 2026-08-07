483daec60c41f6dc74112990fcdb100197aacae6
Javeria Hassan <313981850+javeriahassan-1@users.noreply.github.com>
2026-08-07 20:48:18 +0500

    Add first Python program

---
+++ hello_apex.py
@@ -0,0 +1,12 @@
+print("=" * 60)
+print("🚀 Welcome to Project APEX")
+print()
+
+print("Name :", "Javeria Hassan")
+print("GitHub :", "javeriahassan-1")
+print("Dream Company :", "Porsche / BMW / Formula 1")
+print("Mission :", "Become an engineer who solves real problems with Python")
+
+print()
+print("This is my first Python program.")
+print("=" * 60)
